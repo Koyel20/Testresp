96b320466d6cad04c4930eb989ccb7b4bf714e5a
Koyel20 <45252933+Koyel20@users.noreply.github.com>
2025-09-02 21:18:23 +0530

    Create tokenization.py

--- tokenization.py
+++ tokenization.py
@@ -1,7 +1,4 @@
 from nltk.tokenize import sent_tokenize
-corpus = """hi this is koyel.
-This is a trial for tokenization!
-exclaimation and period are considerred,
-new line"""
+corpus = """hi this is koyel.This is a trial for tokenization!exclaimation and period are considerred,new line."""
 
 sent_tokenize(corpus)
