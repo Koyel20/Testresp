1df18a7c90f083fa7d12355f362a96116f6839e2
Koyel20 <45252933+Koyel20@users.noreply.github.com>
2025-09-02 21:08:33 +0530

    tokenization.py

---
+++ tokenization.py
@@ -0,0 +1,7 @@
+from nltk.tokenize import sent_tokenize
+corpus = """hi this is koyel.
+This is a trial for tokenization!
+exclaimation and period are considerred,
+new line"""
+
+sent_tokenize(corpus)
